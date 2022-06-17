003b675a3821c6772dbefbf28e57b41242b494d7
Quentin Poterek <quentin.poterek@outlook.fr>
2022-06-17 14:10:08 +0200

    Add documentation and refactor map_geometry_type method

--- src/lib.py
+++ src/lib.py
@@ -3,7 +3,7 @@
 # -----------------------------------------------------------------------------
 # Author: Quentin Poterek
 # Creation date: 08/06/2022
-# Version: 1.1
+# Version: 1.2
 #------------------------------------------------------------------------------
 
 
@@ -21,8 +21,32 @@ import geopandas as gpd
 import sqlalchemy
 
 
-def flatten(iterable: Iterable) -> Iterable:
-	return list(itertools.chain(*iterable))
+def flatten(lists: Iterable) -> Iterable:
+	"""
+	Flatten a list of lists into a single non-nested list.
+
+	Parameters
+	----------
+		lists: iterable
+			List of lists.
+	
+	Returns
+	-------
+		iterable
+			Flattened non-nested list.
+	
+	Examples
+	--------
+	Create an irregularly shaped list with nested items.
+
+	>>> my_list = [1, 2, [3, 4], [5, 6, 7], 8, 9]
+
+	Flatten the list. 
+
+	>>> flatten(my_list)
+	[1, 2, 3, 4, 5, 6, 7, 8, 9]
+	"""
+	return list(itertools.chain(*lists))
 
 
 def check_files(
@@ -182,14 +206,19 @@ def has_single_geometry_type(
 	
 	Examples
 	--------
-	Get geometry types from a spatial relation.
+	Create an instance of SqlProcessor.
 
-	>>> connection = psycopg2.connect(...)
-	>>> cursor = connection.cursor()
-	>>> geometry_types = get_geometry_type(cursor, "communes")
+	>>> processor = SqlProcessor(engine, sql_directory)
 
-	Check whether or not the spatial relation contains features with the
-	same geometry type.
+	Get geometry types for features contained in the provided 
+	relation.
+
+	>>> geometry_types = processor.get_geometry_types("communes")
+	>>> geometry_types
+	('ST_MultiPolygon', 'ST_Polygon')
+
+	Check whether or not the spatial relation contains features only one
+	geometry type.
 
 	>>> has_single_geometry_type(geometry_types)
 	False
@@ -218,23 +247,26 @@ def convert_st_to_type(
 	
 	Examples
 	--------
-	Get the geometry type of a spatial relation.
+	Create an instance of SqlProcessor.
 
-	>>> connection = psycopg2.connect(...)
-	>>> cursor = connection.cursor()
-	>>> geometry_types = get_geometry_type(cursor, "communes")
+	>>> processor = SqlProcessor(engine, sql_directory)
+
+	Get geometry types for features contained in the provided 
+	relation.
+
+	>>> geometry_types = processor.get_geometry_types("communes")
 	>>> geometry_types
-	[('ST_MultiPolygon',)]
+	('ST_MultiPolygon',)
 
 	Convert the PostGIS geometry type into a generic form.
 
-	>>> convert_st_to_type(geometry_types[0][0])
+	>>> convert_st_to_type(geometry_types[0])
 	>>> 'MultiPolygon'
 
 	It is also possible to convert a multipart geometry to its
 	singlepart equivalent by setting `allow_multi` to False.
 
-	>>> convert_st_to_type(geometry_types[0][0], false)
+	>>> convert_st_to_type(geometry_types[0], false)
 	>>> 'Polygon'	
 	"""
 	if geometry[:3] != "ST_":
@@ -365,13 +397,24 @@ class Credentials:
 
 class SqlProcessor:
 	"""
+	A class used to do CRUD processing on data by executing SQL queries
+	through an sqlalchemy engine.
 	"""
 	def __init__(
 		self, 
 		engine: sqlalchemy.engine.base.Engine,
-		sql_directory: str
+		sql_directory: Optional[str] = None
 	) -> None:
 		"""
+		Initialize the SQL processor.
+
+		Parameters
+		----------
+			engine : sqlalchemy.engine.base.Engine
+				Valid sqlalchemy engine for connecting to a PostGIS
+				database.
+			sql_directory : str, optional
+				Absolute path where SQL queries are stored.
 		"""
 		self.engine = engine
 		self.sql_directory = sql_directory
@@ -384,57 +427,75 @@ class SqlProcessor:
 		*parameters: Union[str, int, float]
 	) -> None:
 		"""
-		Execute a query from an external SQL file, formatted with input
-		parameters.
+		Execute a query from a string or an external SQL file, 
+		formatted with input parameters.
 
 		Parameters
 		----------
-			engine : sqlalchemy.engine.base.Engine
-				Engine bound to a PostgreSQL database.
-			path : str
-				Either a SQL query or a path to the SQL query file.
 			mode : str
 				Specify whether the query is distributed as a string
 				(string mode) or as a file to read (file mode). This
 				parameter can take the following values: file, string.
+			query : str
 			parameters : iterable of str, int, float
 				Parameters used for formatting the SQL query.
 		
 		Raises
 		------
-			ValueError: If the provided mode is not supported. Supported
-				modes are "file" and "string".
+			ValueError : If the provided mode is not supported. 
+				Supported modes are "file" and "string".
+			ValueError : If a query is executed in `file` mode while
+				`sql_directory = None`.
 
 		Examples
 		--------
-		Create an engine for connecting to a PostgreSQL database.
 
+		Provide path where SQL queries are stored and create an engine 
+		for connecting to a PostgreSQL database.
+
+		>>> sql_directory = "/path/to/sql/directory/"
 		>>> engine = sqlalchemy.create_engine(...)
-		>>> cursor = connection.cursor()
 
-		Execute a SQL query from an external file in file mode.
+		Create an instance of SqlProcessor.
 
-		>>> execute_query(
-			engine, "./my_query.sql", 
-			"commune", "INSEE_COM", "97%"
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Execute a SQL query from an external file in file mode. The
+		corresponding file is stored in the `sql_directory`.
+
+		>>> processor.execute_query(
+			"file",                          # Mode
+			"my_query.sql",                  # Query
+			"commune", "INSEE_COM", "97%"    # Parameters
 		)
 		
-		Execute a SQL query from a string in string mode.
+		Execute a SQL query from a string in string mode. 
+		
+		Note: When using string mode exclusively, it is not necessary to
+		provide a value for `sql_directory` when creating an instance of
+		SqlProcessor.
 		
 		>>> query = '''
-		DELETE FROM public."%s"
-		WHERE "%s" LIKE '%s';
+		DELETE FROM public."{0}"
+		WHERE "{1}" LIKE '{2}';
 		'''
-		>>> execute_query(
-			engine, query, "string",
-			"commune", "INSEE_COM", "97%"
+		>>> processor.execute_query(
+			"string",                        # Mode
+			query,                           # Query
+			"commune", "INSEE_COM", "97%"    # Parameters
 		)
 		"""
 		
-		if mode not in ("file", "string"):
+		if mode.lower() not in ("file", "string"):
 			raise ValueError(f"{mode} is not a supported mode!")
+		
+		if mode.lower() == "file" and self.sql_directory is None:
+			raise ValueError(
+				"It is not possible to run a query in 'file' mode "
+				"without specifying a SQL directory!"
+			)
 
-		if mode == "file":
+		if mode.lower() == "file":
 			with open(query) as f:
 				query = f.read()
 
@@ -451,6 +512,28 @@ class SqlProcessor:
 		relation: str
 	) -> int:
 		"""
+		Get the SRID of a spatial relation.
+
+		Parameters
+		----------
+			relation : str
+				Name of the relation for which to get the SRID.
+		
+		Returns
+		-------
+			int
+				SRID of the provided relation.
+
+		Examples
+		--------
+		Create an instance of SqlProcessor.
+
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Get the SRID from a spatial relation.
+
+		>>> processor.get_srid(cursor, "communes")
+		2154
 		"""
 		srid = self.execute_query(
 			"file",
@@ -467,6 +550,20 @@ class SqlProcessor:
 		relation_b: str
 	) -> bool:
 		"""
+		Check whether or not input relations share the same SRID.
+
+		Parameters
+		----------
+			relation_a : str
+				Name of a spatial relation.
+			relation_b : str
+				Name of a spatial relation.
+		
+		Returns
+		-------
+			bool
+				Return `True` if both relations share the same SRID,
+				`False` otherwise.
 		"""
 		srid_a = self.get_srid(relation_a)
 		srid_b = self.get_srid(relation_b)
@@ -479,6 +576,22 @@ class SqlProcessor:
 		relation: str
 	) -> None:
 		"""
+		Create a spatial index for the input spatial relation.
+
+		Parameters
+		----------
+			relation : str
+				Name of the relation for which to create an index.
+		
+		Examples
+		--------
+		Create an instance of SqlProcessor.
+
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Create a spatial index for the provided relation.
+
+		>>> processor.create_spatial_index("bd_topo")
 		"""
 		self.execute_query(
 			"file",
@@ -492,6 +605,34 @@ class SqlProcessor:
 		relation: str
 	) -> None:
 		"""
+		Convert the singlepart features of an input spatial relation to 
+		multipart features.
+
+		Parameters
+		----------
+			relation : str
+				Name of the relation for which to convert singlepart 
+				features to multipart features.
+		
+		Examples
+		--------
+		Create an instance of SqlProcessor.
+
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Get the geometry type of a spatial relation.
+
+		>>> processor.get_geometry_types("communes")
+		('ST_Polygon',)
+
+		Convert singleipart geometry to multipart geometry.
+
+		>>> processor.single_to_multi_geometry("communes")
+
+		Get the geometry type of the converted spatial relation.
+
+		>>> processor.get_geometry_types("communes")
+		('ST_MultiPolygon',)
 		"""
 		self.execute_query(
 			"file",
@@ -500,20 +641,98 @@ class SqlProcessor:
 		)
 
 
+
+	def get_geometry_types(
+		self,
+		relation: str
+	) -> Iterable[str]:
+		"""
+		Get distinct geometry types existing in a spatial relation.
+
+		Parameters
+		----------
+			relation : str
+				Name of the relation for which to get geometry types.
+
+		Returns
+		-------
+			iterable of str
+				Iterable storing the distinct geometry types found in a
+				spatial relation.
+		
+		Examples
+		--------
+		Create an instance of SqlProcessor.
+
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Get geometry types for features contained in the provided 
+		relation.
+
+		>>> processor.get_geometry_types("communes")
+		('ST_MultiPolygon',)
+		"""
+		return self.execute_query(
+			"file",
+			join(self.sql_directory, "select_distinct_geometry_type.sql"),
+			relation
+		)	
+
+
 	def map_geometry_type(
 		self,
 		relation: str,
 		allow_multi: bool = True
 	) -> str:
 		"""
+		Get geometry type in a spatial relation and convert it to a
+		generic type.
+
+		Parameters
+		----------
+			relation : str
+				Spatial relation from which to get and convert the 
+				geometry type.
+			allow_multi : bool, default True
+				Whether or not to allow multipart geometries. If `False`
+				the multipart geometry type is converted to its
+				singlepart equivalent.
+		
+		Returns
+		-------
+			str
+				Generic geometric, converted from a PostGIS geometry
+				type.
+		
+		Raises
+		------
+			ValueError : When a spatial relation has more than one
+				distinct geometry type.
+		
+		Examples
+		--------
+		Create an instance of SqlProcessor.
+
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Get geometry types for features contained in the provided 
+		relation.
+
+		>>> processor.get_geometry_types("communes")
+		('ST_MultiPolygon',)
+
+		Convert the PostGIS geometry type to a generic form.
+
+		>>> processor.map_geometry_type("communes")
+		"MultiPolygon"
+
+		Convert the PostGIS geometry type to a generic singlepart form.
+
+		>>> processor.map_geometry_type("communes", allow_multi=False)
+		"Polygon"
 		"""
 		# Get geometry type(s) from relation
-		geometry_types = self.execute_query(
-			"file",
-			join(self.sql_directory, "select_distinct_geometry_type.sql"),
-			relation
-		)
-
+		geometry_types = self.get_geometry_types(relation)
 		geometry_types = flatten(geometry_types)
 
 		# Map geometry type when possible
@@ -533,6 +752,32 @@ class SqlProcessor:
 		srid: Union[int, str]
 	) -> None:
 		"""
+		Project the geometries of an input spatial relation to the 
+		provided SRID.
+
+		Parameters
+		----------
+			relation : str
+				Name of the relation for which to get the SRID.
+			srid : str or int
+				Target SRID.
+		
+		Examples
+		--------
+		Create an instance of SqlProcessor.
+
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Get the SRID of a spatial relation.
+
+		>>> processor.get_srid("communes")
+		4326
+
+		Reproject data from EPSG:4326 to EPSG:2154.
+
+		>>> processor.project_geometry("communes", 2154)
+		>>> processor.get_srid("communes")
+		2154	
 		"""
 		# Get standard geometry type from relation
 		geometry_type = self.map_geometry_type(relation)
@@ -551,6 +796,24 @@ class SqlProcessor:
 		srid: Union[int, str]
 	) -> None:
 		"""
+		Remove the third dimension of all features in a spatial 
+		relation.
+
+		Parameters
+		----------
+			relation : str
+				Name of the relation for which to remove the third 
+				dimension.
+		
+		Examples
+		--------
+		Create an instance of SqlProcessor.
+
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Remove the third dimension from a spatial relation.
+
+		>>> processor.transform_3d_to_2d("communes")
 		"""
 		# Get standard geometry type from relation
 		geometry_type = self.map_geometry_type(relation)
@@ -579,8 +842,6 @@ class SqlProcessor:
 
 		Parameters
 		----------
-			cursor : psycopg2.extensions.cursor
-				Cursor bound to an open PostgreSQL connection.
 			relation_a : str
 				Relation containing geometries to intersect with
 				`relation_b`.
@@ -604,30 +865,28 @@ class SqlProcessor:
 		
 		Examples
 		--------
-		Create a cursor from an open PostgreSQL connection.
+		Create an instance of SqlProcessor.
 
-		>>> connection = psycopg2.connect(...)
-		>>> cursor = connection.cursor()
+		>>> processor = SqlProcessor(engine, sql_directory)
 
 		Update the geometry of spatial relation `b` after intersecting
 		it with spatial relation `a`. In this following example, the
 		only information kept from both relations is the resulting
 		geometry.
 
-		>>> intersect_geometries(cursor, "a", "b")
+		>>> processor.intersect_geometries("a", "b")
 
 		To create a new relation `c` instead of overwriting `b`, one may
 		specify the `out_name` attribute.
 
-		>>> intersect_geometries(cursor, "a", "b", out_name="c")
+		>>> processor.intersect_geometries("a", "b", out_name="c")
 
 		A table is created by default. However, when `out_name` is
 		provided, it is possible to set `as_view` to `True` in order to
 		create a view instead.
 
-		>>> intersect_geometries(
-			cursor, "a", "b", 
-			as_view=True, out_name="c"
+		>>> processor.intersect_geometries(
+			"a", "b", as_view=True, out_name="c"
 		)
 
 		In addition to the geometry resulting from the intersection of
@@ -642,8 +901,8 @@ class SqlProcessor:
 		attributes from `b` are kept. Note that the initial geometry of
 		both relations was not included.
 
-		>>> intersect_geometries(
-			cursor, "a", "b", 
+		>>> processor.intersect_geometries(
+			"a", "b", 
 			fields_a=["id"], 
 			fields_b=["x", "y", "z"]
 		)
@@ -706,36 +965,37 @@ class SqlProcessor:
 		Parameters
 		----------
 			relation_a : str
-				Relation containing geometries to aggregate with relation_b.
+				Relation containing geometries to aggregate with 
+				`relation_b`.
 			relation_b : str
-				Relation containing geometries to aggregate with relation_a.
+				Relation containing geometries to aggregate with 
+				`relation_a`.
 			out_name : str
 				Name of the relation to create.
 			as_view : bool, default False
 				Whether or not to create a view. When False, a table is 
 				created. When True, a view is created.
 			build_index : bool, default True
-				Whether or not to build a spatial index for the aggregation
-				result. The option is not available for views.
+				Whether or not to build a spatial index for the 
+				aggregation result. The option is not available for 
+				views.
 		
 		Examples
 		--------
-		Create a cursor from an open PostgreSQL connection.
+		Create an instance of SqlProcessor.
 
-		>>> connection = psycopg2.connect(...)
-		>>> cursor = connection.cursor()
+		>>> processor = SqlProcessor(engine, sql_directory)
 
 		Create a new spatial relation `c` from the union of two spatial
 		relations `a` and `b`.
 
-		>>> aggregate_relations(cursor, "a", "b", "c")
+		>>> processor.aggregate_relations("a", "b", "c")
 
 		By default, a new table is created. But one might also want to
 		create a view. In this case, `as_view` must be set to `True`.
 
-		>>> aggregate_relations(cursor, "a", "b", "c", as_view=True)
+		>>> processor.aggregate_relations("a", "b", "c", as_view=True)
 		"""
-
 		if not self.is_same_srid(relation_a, relation_b):
 			raise ValueError(f"Input geometries do not share the same SRID!")
 		
@@ -753,23 +1013,51 @@ class SqlProcessor:
 			self.create_spatial_index(out_name)
 
 
-	def clear_relations(
+	def drop_relations(
 		self,
 		relation_type: str,
 		relations: Iterable[str]
 	) -> None:
 		"""
-		"""
+		Drop input relations, whether tables or views.
 
+		Parameters
+		----------
+			relation_type : str
+				Specify whether the relations are `TABLE`s or `VIEW`s.
+			relations : iterable of str
+				Relations to drop.
+		
+		Raises
+		------
+			ValueError : The provided `relation_type` is invalid.
+				Supported values are `TABLE` and `VIEW`.
+			ValueError : A relation could not be dropped. In this case,
+				sqlalchemy's error is caught and displayed to the user.
+
+		Examples
+		--------
+		Create an instance of SqlProcessor.
+
+		>>> processor = SqlProcessor(engine, sql_directory)
+
+		Drop relations.
+
+		>>> processor.drop_relations("TABLE", ["a", "b", "c"])
+		"""
 		if relation_type.upper() not in ("TABLE", "VIEW"):
 			raise ValueError(
-				"{relation_type} is not a valid relation type! "
+				f"{relation_type} is not a valid relation type! "
 				"Valid values are 'TABLE' or 'VIEW'."
 			)
 
 		for relation in (relations):
-			self.execute_query(
-				"file",
-				join(self.sql_directory, "drop_relation.sql"), 
-				relation_type, relation, "CASCADE"
-			)
+			try:
+				self.execute_query(
+					"file",
+					join(self.sql_directory, "drop_relation.sql"), 
+					relation_type, relation, "CASCADE"
+				)
+			except Exception as e:
+				raise ValueError(f"{relation} could not be dropped!")
+				print(e)
