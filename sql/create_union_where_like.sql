9e5534a3eb9c684fbe225e7ee1de9f5a60c4daf9
Quentin Poterek <quentin.poterek@outlook.fr>
2022-06-21 14:32:42 +0200

    Fix issue with multipart geometry

--- sql/create_union_where_like.sql
+++ sql/create_union_where_like.sql
@@ -1,6 +1,6 @@
 CREATE {0} public."{1}" AS (
 	SELECT ROW_NUMBER() OVER () AS id,
-	       ST_Union(geometry)::geometry(Polygon, {2}) AS geometry
+	       ST_Multi(ST_Union(geometry))::geometry(MultiPolygon, {2}) AS geometry
 	FROM public."{3}"
 	WHERE "{4}" LIKE '{5}'
 );
